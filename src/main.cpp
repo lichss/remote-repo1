5afb6a114bd84e7d07ff27674cf270d6bcdaf0a6
xcs <evtjk@hotmail.com>
2024-06-20 16:44:24 +0800

    dev: 1st commit

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@ const char* path = "files\\records";
 void menu();
 void list_workers(Worker_base* workers[]);
 void sort_workers(Worker_base* workers[]);
+void edit_workers(Worker_base* workers[]);
 
 int testwrite()
 {
@@ -33,44 +34,61 @@ int testwrite()
     std::cout <<"save down\n";
     return 0;
 }
-void add_worker(Worker_base *workers[], int index, std::string name, int id)
+void add_workers(Worker_base *workers[])
 {
     using std::cin;
     using std::cout;
     using std::endl;
     using std::string;
 
+    int index;
+    string name;
+    int id;
     string function = "yuangong";
+
+    cout << "enter add mode.\n";
+    cout << "new index:_____\b\b\b";
+    cin >> index;
+    cout << "new name:_____\b\b\b";
+    cin >> name;
+    cout << "new id:_____\b\b\b";
+    cin >> id;
+    cout << "new function:_____\b\b\b";
+    cin >> function;
+
+
+    
 
     if (function == "yuangong"){
         workers[g_workers_index] = new Worker_e(index, name, id);
         g_workers_index++;
 
-        std::cout << id << "\tread\t" << function << std::endl;
+        std::cout << id << "\tcreat\t" << function << std::endl;
     }
 
     else if (function == "jingli"){
         workers[g_workers_index] = new Worker_m(index, name, id);
         g_workers_index++;
 
-        std::cout << id << "\tread\t" << function << std::endl;
+        std::cout << id << "\tcreat\t" << function << std::endl;
     }
 
     else if (function == "Boss"){
         workers[g_workers_index] = new Worker_b(index, name, id);
         g_workers_index++;
 
-        std::cout << id << "\tread\t" << function << std::endl;
+        std::cout << id << "\tcreat\t" << function << std::endl;
     }
 }
 
-void delete_worker(Worker_base* workers[],int delete_index)
+void delete_workers(Worker_base* workers[],int delete_index)
 {
     for(int i=0;i<g_workers_index;i++){ /*遍历指针组查找删除目标*/
         if(workers[i]!=nullptr){    /*排除已删除目标*/
             if(workers[i]->m_Index_n == delete_index){  /*第一个符合删除索引的目标*/
                 delete workers[i];
                 workers[i] = nullptr;
+                std::cout<<"find  index"<<delete_index<<"\n";
                 std::cout<<"index "<<delete_index<<" deleted\n";
             }
         else
@@ -141,32 +159,99 @@ void test_read_c(Worker_base* workers[])
 
 int main()
 {
-    // menu();
-    // char c = '\0';
-    // while(std::cin>>c){
-    //     std::cout<<"get:"<<c<<"\n";
+    Worker_base* workers[99] = {0,};
+    test_read_c(workers);
 
-    // }
+    menu();
+    char c = '\0';
+    while(std::cin>>c){
+        std::cout<<"get:"<<c<<"\n";
+        switch (c)
+        {
+        case '0':
+            break; 
+        case '1'/* constant-expression */:
+            add_workers(workers);
+            break;
+        case '2'/* constant-expression */:
+            list_workers(workers);
+            break;
+        case '3'/* constant-expression */:
+            list_workers(workers);
+            break;
+        case '4'/* constant-expression */:
+            list_workers(workers);
+            break;
+        case '5'/* constant-expression */:
+            list_workers(workers);
+            break;
+        case '6'/* constant-expression */:
+            list_workers(workers);
+            break;
+        case '7'/* constant-expression */:
+            add_workers(workers);
+            break;
+        
+        default:
+            break;
+        }
+        menu();
+
+    }
     // init_record();
 
-    Worker_base* workers[99] = {0,};
 
     // testwrite();
     test_read_c(workers);
     //add_worker(workers,14,"DDD",936);
     
     
-    // delete_worker(workers,2);
+    delete_workers(workers,2);
     // delete_worker(workers,5);
     // delete_worker(workers,11);
 
     
-    list_workers(workers);
+    
     sort_workers(workers);
-    std::cout<<"\n\nsort\n";
-    list_workers(workers);
+    
+    
 
     return 0;
+}
+void edit_workers(Worker_base* workers[])
+{
+    using std::cin;
+    using std::cout;
+    using std::endl;
+    using std::string;
+
+    int edit_index;
+    int is_found = 0;
+    cout << "enter edit mode.\nindex: ";
+    cin >> edit_index;
+    for(int  i=0;i<g_workers_index;i++)
+        if(workers[i]!=nullptr){
+            if(workers[i]->m_Index_n == edit_index){
+                is_found = 1;
+                break;/*如果存在多个index 只修改排列顺序第一个*/
+            }
+        } 
+    if(is_found){
+        int new_index;
+        string new_name;
+        int new_id;
+        string new_function;
+
+        cout << "\nnew index:  ";
+        cin  >> new_index;
+        cout << "\nnew name:  ";
+        cin  >> new_name;
+        cout << "\nnew id:  ";
+        cin  >> new_id;
+    }
+    else{
+        cout<<"\n can not find index:"<<edit_index<<endl;
+    }
 }
 
 void list_workers(Worker_base* workers[])
@@ -213,16 +298,13 @@ void sort_workers(Worker_base* workers[])
 
     int index_i;
     int index_i_last = 0;
-    int debug_n=0;
-    int debug_c=0;
+
     for (int j=0;j<g_workers_index;j++){
         for(int i=0;i<g_workers_index;i++){
             if(workers[i]!=nullptr){
                 index_i_last = index_i;
                 index_i = i;
-                ++debug_n;
-                // if(debug_n==13)
-                //     debug_n=0;
+
                 sordex_i_last = sordex_i;
                 sordex_i = workers[i]->m_Index_n;
                 if(sordex_i < sordex_i_last){   /*有效顺序中出现逆序*/
@@ -238,12 +320,12 @@ void sort_workers(Worker_base* workers[])
             //
             else{
                 continue;
-                debug_c++;
                 }
         }
         index_i = j;
         index_i_last = index_i;
-        sordex_i = 0;
+        sordex_i = 0; // 这一行有一个潜在的bug  如果worker[0] == nulptr 程序可能要歇逼
         sordex_i_last = -1;
     }
 }
+
